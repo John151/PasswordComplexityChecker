1d77aa3979873e202e1a132f30eb3641b8ab1b8e
John Cokins <il7148pg@go.minnstate.edu>
2020-02-10 19:43:40 -0600

    Add project files.

---
+++ PasswordComplexityChecker/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace PasswordComplexityChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtStrengthResult = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(84, 39);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // txtStrengthResult
+            // 
+            this.txtStrengthResult.AutoSize = true;
+            this.txtStrengthResult.Location = new System.Drawing.Point(81, 153);
+            this.txtStrengthResult.Name = "txtStrengthResult";
+            this.txtStrengthResult.Size = new System.Drawing.Size(93, 13);
+            this.txtStrengthResult.TabIndex = 2;
+            this.txtStrengthResult.Text = "password strength";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(66, 98);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(129, 23);
+            this.btnCheck.TabIndex = 3;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 220);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtStrengthResult);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label txtStrengthResult;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ PasswordComplexityChecker/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordComplexityChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+           //converts txtPassword input into a string, checks its length
+            string PasswordString = Convert.ToString(txtPassword.Text);
+            int passwordLength = PasswordString.Length;
+
+            if (passwordLength <= 8)
+            {
+                txtStrengthResult.Text = "Password is too weak";
+            }
+            if (passwordLength > 8 && passwordLength < 15)
+            {
+                txtStrengthResult.Text = "Password is acceptable";
+            }
+            if (passwordLength >= 15)
+            {
+                txtStrengthResult.Text = "Password is good";
+            }
+            //resets txtPassword to check another password
+            txtPassword.Text = "";
+        }
+    }
+}
